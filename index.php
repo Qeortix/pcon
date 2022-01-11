8e3d7edd63b6f90768d54fe2313befabc620f672
Qeortix <94023668+Qeortix@users.noreply.github.com>
2022-01-11 20:16:50 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Holtwood+One+SC&family=Orelega+One&family=Yanone+Kaffeesatz&display=swap"
+            rel="stylesheet"
+        />
+        <link rel="stylesheet" href="style.css" />
+        <script src="ta.js"></script>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Включен ли ваш компьютер</title>
+    </head>
+    <body>
+        <div class="space">s</div>
+        <div class="smain">
+            <input type="button" value="проверить" onmousedown="viewDiv()" />
+            <div id="div1">Ваш компьютер включен</div>
+        </div>
+    </body>
+</html>
